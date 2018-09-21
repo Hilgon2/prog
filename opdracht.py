e4d382f0b7a8708b14a4744ecb5a72d465be88bd
BuildTools <unconfigured@null.spigotmc.org>
2018-09-21 15:18:21 +0200

    Test commit

---
+++ opdracht.py
@@ -0,0 +1,5 @@
+hourSalary = input("Wat verdien je per uur?")
+hourWorked = input("Hoeveel uur heb je gewerkt?")
+salary = float(hourWorked) * float(hourSalary)
+print(hourWorked + " uur werken levert " + str(salary) + " op")
+print("Git test")
