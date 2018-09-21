a792d1165474f16b8b945ca266eb4b99b4d4e1b8
BuildTools <unconfigured@null.spigotmc.org>
2018-09-21 15:53:10 +0200

    Message

---
+++ PE2_2.py
@@ -0,0 +1,7 @@
+print(len('Supercalifragilisticexpialidocious'))
+print('ice' in 'Supercalifragilisticexpialidocious')
+print(len('Antidisestablishmentarianism') > len('Honorificabilitudinitatibus'))
+componisten = ['Berlioz', 'Borodin', 'Brian', 'Bartok', 'Bellini', 'Buxtehude', 'Bernstein']
+componisten.sort()
+print(componisten[0])
+print(componisten[-1])
